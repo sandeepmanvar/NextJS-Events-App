4a1ec18655c9e1cea378b50b1719e337fc39a150
Sandeep M <sandeep.manvar@outlook.com>
2021-05-23 17:33:25 +0530

    Updated events page and make it functional with dummy data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,12 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
 function AllEventsPage() {
-    return (
-        <div>
-            <h1>Events Page</h1>
-        </div>
-    );
+  const events = getAllEvents();
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
 }
 
 export default AllEventsPage;
