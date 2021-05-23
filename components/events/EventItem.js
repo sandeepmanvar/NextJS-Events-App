a0560d53a17ed26df219635498eb21cf6ac14a85
Sandeep M <sandeep.manvar@outlook.com>
2021-05-23 14:31:43 +0530

    Added styles to components

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import classes from "./EventItem.module.css";
+
 function EventItem(props) {
   const { title, image, date, location, id } = props;
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -11,21 +13,21 @@ function EventItem(props) {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
         </div>
-        <div>
-          <time>{humanReadableDate}</time>
+        <div className={classes.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
         </div>
-        <div>
-          <address>{formattedAddress}</address>
-        </div>
-      </div>
-      <div>
-        <Link href={exploreLink}>Explore Event</Link>
       </div>
     </li>
   );
