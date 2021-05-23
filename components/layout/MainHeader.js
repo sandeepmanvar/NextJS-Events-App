eb74aaaa5c9078ccfef19f423d90690fa07734be
Sandeep M <sandeep.manvar@outlook.com>
2021-05-23 17:25:20 +0530

    Added a general layout wrapper

---
+++ components/layout/MainHeader.js
@@ -0,0 +1,21 @@
+import Link  from "next/link";
+import classes from "./MainHeader.module.css";
+
+function MainHeader(props) {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/">Next Event</Link>
+      </div>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href="/events">Browse All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainHeader;
